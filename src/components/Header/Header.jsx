df6e3e3c177292b983043fc98728719811ef7cbc
Hugo Leung <132837475+hugolyy420@users.noreply.github.com>
2024-05-21 00:09:27 +0800

    doc: change header icon src

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,14 @@
 import { NavLink, Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import headerIcon from './default-monochrome.svg';
 
 export const Header = ({ cartItems }) => {
   return (
     <header className="flex justify-between items-center py-4 px-8 sticky top-0 bg-white shadow-md z-50">
       <Link to="/">
         <div className="w-32 h-16 flex items-center">
-          <img src="../../public/default-monochrome.svg" alt="Home Logo" className="object-fill" />
+          <img src={headerIcon} alt="Home Logo" className="object-fill" />
         </div>
       </Link>
       <nav className="flex gap-10">
